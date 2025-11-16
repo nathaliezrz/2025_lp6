b6207f8fcc37229b075ad6f181f6c05150501460
Nathalie Rodriguez <rodrigueznathalie207@gmail.com>
2025-11-15 22:16:04 -0600

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,147 @@
-import Link from "next/link";
+'use client';
+
+import {useEffect, useState} from "react";
 
 const styles = {
   page: {
-    display: "grid", // Using this to center the items
     alignItems: "center",
     justifyItems: "center",
     gap: "64px",
-    minHeight: "100svh",
   },
   main: {
     display: "flex",
-    flexDirection: "row",
+    flexDirection: "column",
     gap: "32px",
+  },
+  box: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: "20px",
+    justifyContent: "center"
+  },
+  item: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: "10px",
+    width: "100%",
+    borderRadius: "5px",
+    backgroundColor: "blue",
+    padding: "20px",
+    color: "White"
+  },
+  text: {
+    fontFamily: "arial",
+    fontSize: "20px"
   }
 } as const;
 
-export default function Page() {
+type Data = {
+  hourly: {
+    wind_speed_10m: Array<number>
+    time: Array<string>
+  }
+  hourly_units: {
+    wind_speed_10m: string
+  }
+}
+
+export default function WeatherScreen() {
+  const [parsedData, setParsedData] = useState<Data>({hourly: {
+    wind_speed_10m: [],
+    time: [],
+  },
+  hourly_units: {
+    wind_speed_10m: "",
+  }})
+  async function fetchWindSpeed() {
+    const data = await fetch('https://api.open-meteo.com/v1/forecast?latitude=29.72&longitude=-95.4&hourly=wind_speed_10m')
+    const json = await data.json()
+    setParsedData(json as Data)
+  }
+
+  function alterTime() {
+    return parsedData.hourly.time.map((time, index) => {
+      let stringDate: Date = new Date(time)
+      let hours: number = stringDate.getHours()
+      if(hours > 12) {
+        return hours-12 + ":00 PM"
+      }
+      else if(hours == 0){
+        return "12:00 AM"
+      }
+      else if(hours == 12){
+        return "12:00 PM"
+      }
+      return hours + ":00 AM"
+      
+    })
+  }
+
+  function getWindColor(num: number) {
+    if(num > 0 && num < 1){
+      return(
+        'gray'
+      )
+    }
+    else if(num >= 1 && num < 2){
+      return(
+        "yellow"
+      )
+    }
+    else if(num >= 2 && num < 3){
+      return(
+        "green"
+      )
+    }
+      else if(num >= 3 && num < 4){
+      return(
+        "blue"
+      )
+    }
+    else if(num >= 4 && num < 5){
+      return(
+        "purple"
+      )
+    }
+    else{
+      return(
+        "red"
+      )
+    }
+  }
+
+  const [shouldRefresh, setShouldRefresh] = useState(false)
+  // Run once on mount
+  useEffect(() => {
+    fetchWindSpeed();
+  }, [shouldRefresh]);
+
+  const newList = alterTime()
+  
   return (
     <div style={styles.page}>
-      <div style={styles.main}>
-        <PageButton href={"/location"} buttonText={"Location Picker Screen"} />
-        <PageButton href={"/weather"} buttonText={"Weather Screen"} />
-      </div>
+      <main style={styles.main}>
+
+        <div style={styles.box}>
+          {parsedData?.hourly.wind_speed_10m.map((speed, index) => {
+            return(
+              <div key={index} style={{...styles.item, backgroundColor: getWindColor(speed)}}>
+                {newList[index]} 
+                <div style={styles.text}>
+                {speed} {parsedData.hourly_units.wind_speed_10m}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <button onClick={event => {
+          setShouldRefresh(!shouldRefresh);
+        }}>
+          Refresh
+        </button>
+      </main>
     </div>
-  )
-}
-
-interface PageButtonProps {
-  href: string;
-  buttonText: string;
-}
-
-function PageButton(props: PageButtonProps) {
-  return (
-    <Link href={props.href}>
-      <button>{props.buttonText}</button>
-    </Link>
-  )
+  );
 }
--- src/app/weather/page.tsx
+++ src/app/weather/page.tsx
@@ -15,24 +15,47 @@ const styles = {
   }
 } as const;
 
+type Data = {
+  hourly: {
+    temperature_2m: Array<number>
+  }
+  hourly_units: {
+    temperature_2m: string
+  }
+}
+
 export default function WeatherScreen() {
+  const [parsedData, setParsedData] = useState<Data>()
   async function fetchWeather() {
     const data = await fetch('https://api.open-meteo.com/v1/forecast?latitude=29.72&longitude=-95.4&hourly=temperature_2m&forecast_days=1')
     const json = await data.json()
-    // TODO
+    setParsedData(json as Data)
+    //console.log(parsedData.hourly_units.temperature_2m);
   }
 
+  const [shouldRefresh, setShouldRefresh] = useState(false)
   // Run once on mount
   useEffect(() => {
     fetchWeather();
-  }, []);
+  }, [shouldRefresh]);
 
   return (
     <div style={styles.page}>
       <main style={styles.main}>
         <ul>
-          {/*TODO*/}
+          {parsedData?.hourly.temperature_2m.map((temperature, index) => {
+            return(
+              <li key={index}>
+                Temperature: {temperature} {parsedData.hourly_units.temperature_2m}
+              </li>
+            )
+          })}
         </ul>
+        <button onClick={event => {
+          setShouldRefresh(!shouldRefresh);
+        }}>
+          Refresh
+        </button>
       </main>
     </div>
   );
